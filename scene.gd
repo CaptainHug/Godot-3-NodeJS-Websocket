27d0ae910dea7c00f9234ad8217ca35831e3cd76
Melech <wardell.c@gmail.com>
2018-04-17 12:27:38 -0400

    Updated Godot Scripts
    Corrected 2 bugs (connect() -> connect_to_host() and is_connected() -> get_status()==STATUS_CONNECTED)
    
    Disabled masking of data (changed 0x80 to 0x00, and commented_out the put_32 command, replacing masked[i] with utf8msg[i])
    
    Running as a thread caused it to crash on exit (infinite while: loop). I changed this to call the run function during _process in scene.gd
    
    Moved sending of messages to happen immediately rather than waiting for the run() loop to send them (run loop is now only for receiving messages)
    
    renamed start_read -> received_header for clarity (headers are sent first and only once)
    
    Removed extra header information that does not need to be sent for basic connection to nodejs
    
    Corrected spelling (recieve -> receive)

--- scene.gd
+++ scene.gd
@@ -1,35 +1,20 @@
-
 extends Node
 
-#var peer = StreamPeerTCP.new()
 var websocket
-
 var run_time = 0
 
 func _process(delta):
-	#print(peer.is_connected())
-	#print(peer.get_status())
 	run_time += delta
 	get_node("time").set_text(str(run_time).pad_decimals(2))
+	websocket.run()
 
 func _ready():
-	#peer.connect('127.0.0.1',3001)
-	
-	set_process(true)
-	
-	print( get_tree() )
-	
 	websocket = preload('websocket.gd').new(self)
-	websocket.start('godot-websocket-tutorial-marcosbitetti.c9users.io',80)
-	websocket.set_reciever(self,'_on_message_recieved')
+	websocket.start('godot-websocket-tutorial-marcosbitetti.c9users.io',80)  # Replace with site:port for your node.js script
+	websocket.set_receiver(self,'_on_message_received')
 
-func _on_message_recieved(msg):
+func _on_message_received(msg):
 	print(msg)
-
 
 func _on_Button_pressed():
 	websocket.send("Hi server")
-	print('cl')
-
-
-
--- websocket.gd
+++ websocket.gd
@@ -1,14 +1,8 @@
-
 extends StreamPeerTCP
 
-const MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-const USER_AGENT = "Godot-client"
+const MESSAGE_RECEIVED = "msg_received"
+const BINARY_RECEIVED = "binary_received"
 
-
-const MESSAGE_RECIEVED = "msg_recieved"
-const BINARY_RECIEVED = "binary_recieved"
-
-var thread = Thread.new()
 var host = '127.0.0.1'
 var host_only = host
 var path = null
@@ -16,96 +10,24 @@ var port = 80
 var TIMEOUT = 30
 var error = ''
 var messages = []
-var reciever = null
-var reciever_f = null
-var reciever_binary = null
-var reciever_binary_f = null
+var receiver = null
+var receiver_f = null
+var receiver_binary = null
+var receiver_binary_f = null
+var received_header = false
 
 var close_listener = Node.new()
 var dispatcher = Reference.new()
 
-func _run(_self):
-	###
-	# Handshake
-	###
+func run():
 	var tm = 0.0
-	
-	# connect
-	while true:
-		if get_status()==STATUS_ERROR:
-			error = 'Connection fail'
-			return
-		if get_status()==STATUS_CONNECTED:
-			break
-		tm += 0.1
-		if tm>TIMEOUT:
-			error = 'Connection timeout'
-			return
-		OS.delay_msec(100)
-	
-	var _host = self.host
-	if self.port != 80:
-		_host += ':' + str(self.port)
-	var header = ''
 	var data = ''
-	
-	
-	header  = "GET /"+self.path+" HTTP/1.1\r\n"
-	header += "Host: "+self.host_only+"\r\n"
-	header += "Connection: Upgrade\r\n"
-	header += "Pragma: no-cache\r\n"
-	header += "Cache-Control: no-cache\r\n"
-	header += "Upgrade: websocket\r\n"
-	#header += "Origin: http://127.0.0.1:3001\r\n"
-	header += "Sec-WebSocket-Version: 13\r\n"
-	header += "User-Agent: "+USER_AGENT+"\r\n"
-	header += "Accept-Encoding: gzip, deflate, sdch\r\n"
-	header += "Accept-Language: "+str(OS.get_locale())+";q=0.8,en-US;q=0.6,en;q=0.4\r\n"
-	#header += "Sec-WebSocket-Key: "+send_secure+"\r\n"
-	header += "Sec-WebSocket-Key: 6Aw8vTgcG5EvXdQywVvbh_3fMxvd4Q7dcL2caAHAFjV\r\n"
-	header += "Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits\r\n"
-	header += "\r\n"
-	#print(header)
-	
-	if OK!=put_data( header.to_ascii() ):
-		print('error sending handshake headers')
-		return
-
-	data = ''
-	tm = 0.0
-	var start_read = false
-	while true:
-		if get_available_bytes()>0 and not start_read:
-			data += get_string(get_available_bytes())
-			start_read = true
-		elif get_available_bytes()==0 and start_read:
-			break
-		
-		OS.delay_msec(100)
-		tm += 0.1
-		if tm>TIMEOUT:
-			print('timeout')
-			return
-	#print(data)
-
-	var connection_ok = false
-	for lin in data.split("\n"):
-		if lin.find("HTTP/1.1 101")>-1:
-			connection_ok = true
-		# other headers can by cheched here
-	
-	if not connection_ok:
-		#print(data)
-		print("Not connection ok")
-		return
-	
-	data = ''
 	var is_reading_frame = false
 	var size = 0
 	var byte = 0
 	var fin = 0
 	var opcode = 0
-	while is_connected():
+	if get_status()==STATUS_CONNECTED:
 		if get_available_bytes()>0:
 			if not is_reading_frame:
 				# frame
@@ -117,14 +39,12 @@ func _run(_self):
 				var payload = byte & 0x7F
 				#printt('length', get_available_bytes())
 				#printt(fin,mskd,opcode,payload)
-				#if fin:
-				#data += get_string(get_available_bytes())
 				if payload<126:
 					# size of data = payload
 					data += get_string(payload)
 					if fin:
-						if reciever:
-							dispatcher.emit_signal(MESSAGE_RECIEVED, data)
+						if receiver:
+							dispatcher.emit_signal(MESSAGE_RECEIVED, data)
 						data = ''
 				else:
 					size = 0
@@ -140,8 +60,8 @@ func _run(_self):
 						size = 0
 						data += get_string(get_available_bytes())
 						if fin:
-							if reciever:
-								dispatcher.emit_signal(MESSAGE_RECIEVED, data)
+							if receiver:
+								dispatcher.emit_signal(MESSAGE_RECEIVED, data)
 							data = ''
 			else:
 				if size<=get_available_bytes():
@@ -149,36 +69,32 @@ func _run(_self):
 					data += get_string(get_available_bytes())
 					is_reading_frame = false
 					if fin:
-						if reciever:
-							dispatcher.emit_signal(MESSAGE_RECIEVED, data)
+						if receiver:
+							dispatcher.emit_signal(MESSAGE_RECEIVED, data)
 						data = ''
 				else:
 					size -= get_available_bytes()
 					data += get_string(get_available_bytes())
-		
-		# message to send?
-		while messages.size()>0:
-			var msg = messages[0]
-			messages.pop_front()
-			
-			# mount frame
-			var byte = 0x80 # fin
-			byte = byte | 0x01 # text frame
-			put_8(byte)
-			byte = 0x80 | msg.length() # mask flag and payload size
-			put_u8(byte)
-			byte = randi() # mask 32 bit int
-			put_32(byte)
-			var masked = _mask(byte,msg)
-			for i in range(masked.size()):
-				put_u8(masked[i])
-			print(msg+" sent")
-			
-		OS.delay_msec(3)
-	
+
+
 
 func send(msg):
-	messages.append(msg)
+	var byte = 0x80 # fin
+	byte = byte | 0x01 # text frame
+	put_8(byte)
+	byte = 0x00 | msg.length() # mask flag and payload size  **max of 125 bytes
+	put_u8(byte)
+	byte = randi() # mask 32 bit int
+#	put_32(byte)
+#	var masked = _mask(byte,msg)
+#	for i in range(masked.size()):
+#		put_u8(masked[i])
+	# Convert to int array, stream 1 char at a time
+	var utf8msg = msg.to_utf8()
+	for i in range(utf8msg.size()):
+		put_u8(utf8msg[i])
+	print(msg+" sent")
+
 
 
 func start(host,port,path=null):
@@ -190,40 +106,85 @@ func start(host,port,path=null):
 		self.host = host+"/"+path
 	self.path = path
 	self.port = port
-	set_big_endian(true)
-	print(IP.get_local_addresses())
-	if OK==connect(IP.resolve_hostname(host),port):
-		thread.start(self,'_run', self)
+	
+	# Attempt connection to server
+	if OK == connect_to_host(IP.resolve_hostname(host),port):
+		# Wait for connection to be established
+		var tm = 0.0
+		while true:
+			if get_status()==STATUS_ERROR:
+				error = 'Connection fail'
+				return
+			if get_status()==STATUS_CONNECTED:
+				break
+			tm += 0.01
+			if tm>TIMEOUT:
+				error = 'Connection timeout'
+				return
+			OS.delay_msec(10)
+		
+		# Prepare header
+		var header  = "GET / HTTP/1.1\r\n"
+		header += "Host: " + host + "\r\n"
+		header += "Upgrade: websocket\r\n"
+		header += "Connection: Upgrade\r\n"
+		header += "Sec-WebSocket-Key: 6Aw8vTgcG5EvXdQywVvbh_3fMxvd4Q7dcL2caAHAFjV\r\n"
+		header += "Sec-WebSocket-Version: 13\r\n"
+		header += "\r\n"
+		
+		# Send header
+		if OK != put_data(header.to_ascii()):
+			print('error sending handshake headers')
+			return
+	
 	else:
 		print('no')
-
-func set_reciever(o,f):
-	if reciever:
-		unset_reciever()
-	reciever = o
-	reciever_f = f
-	dispatcher.connect( MESSAGE_RECIEVED, reciever, reciever_f)
-
-func set_binary_reciever(o,f):
-	if reciever_binary:
-		unset_binary_reciever()
-	reciever_binary = o
-	reciever_binary_f = f
-	dispatcher.connect( MESSAGE_RECIEVED, reciever_binary, reciever_binary_f)
-
-func unset_reciever():
-	dispatcher.disconnect( MESSAGE_RECIEVED, reciever, reciever_f)
-	reciever = null
-	reciever_f = null
-
-func unset_binary_reciever():
-	dispatcher.disconnect( MESSAGE_RECIEVED, reciever_binary, reciever_binary_f)
-	reciever_binary = null
-	reciever_binary_f = null
 	
+	# Received header information is parsed differently
+	var data = ''
+	var tm = 0.0
+	while !received_header:
+		if get_available_bytes() > 0:
+			data += get_string(get_available_bytes())
+			received_header = true
+			#print(data)
+			return
+		
+		OS.delay_msec(100)
+		tm += 0.1
+		if tm>TIMEOUT:
+			print('timeout')
+			return
+
+
+
+func set_receiver(o,f):
+	if receiver:
+		unset_receiver()
+	receiver = o
+	receiver_f = f
+	dispatcher.connect( MESSAGE_RECEIVED, receiver, receiver_f)
+
+func set_binary_receiver(o,f):
+	if receiver_binary:
+		unset_binary_receiver()
+	receiver_binary = o
+	receiver_binary_f = f
+	dispatcher.connect( MESSAGE_RECEIVED, receiver_binary, receiver_binary_f)
+
+func unset_receiver():
+	dispatcher.disconnect( MESSAGE_RECEIVED, receiver, receiver_f)
+	receiver = null
+	receiver_f = null
+
+func unset_binary_receiver():
+	dispatcher.disconnect( MESSAGE_RECEIVED, receiver_binary, receiver_binary_f)
+	receiver_binary = null
+	receiver_binary_f = null
+
 func _init(reference).():
-	dispatcher.add_user_signal(MESSAGE_RECIEVED)
-	dispatcher.add_user_signal(BINARY_RECIEVED)
+	dispatcher.add_user_signal(MESSAGE_RECEIVED)
+	dispatcher.add_user_signal(BINARY_RECEIVED)
 
 func _mask(_m, _d):
 	_m = int_to_hex(_m)
