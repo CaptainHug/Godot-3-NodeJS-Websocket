c51e275960535abfa3595311e6bce2852f03d478
Melech <wardell.c@gmail.com>
2018-04-17 12:35:25 -0400

    Fixed port

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var http = require('http'),
 var server = http.createServer(function(req, res) {
 	res.writeHead(200, { 'Content-type': 'text/html'});
 	res.end(fs.readFileSync(__dirname + '/public/index.html'));
-}).listen(8017, function() {
+}).listen(3000, function() {
 	console.log('Listening at: http://localhost:3000');
 });
 
